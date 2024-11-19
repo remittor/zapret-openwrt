5f584bcf5893e4efb7a7fba8d8d7e7e6f57b879a
remittor <remittor@gmail.com>
2024-11-19 21:09:36 +0300

    config: Renew default config (v69)
    Source: https://github.com/bol-van/zapret-win-bundle/blob/master/zapret-winws/preset_russia.cmd

--- zapret/def-cfg.sh
+++ zapret/def-cfg.sh
@@ -34,12 +34,13 @@ function set_cfg_default_values
 		set $cfgname.config.NFQWS_PORTS_UDP_KEEPALIVE='0'
 		set $cfgname.config.NFQWS_OPT="
 			--filter-tcp=80 <HOSTLIST>
-			--dpi-desync=fake,split2
+			--dpi-desync=fake,fakedsplit
 			--dpi-desync-autottl=2
 			--dpi-desync-fooling=md5sig
 			--new
 			--filter-tcp=443 --hostlist=/opt/zapret/ipset/zapret-hosts-google.txt
-			--dpi-desync=fake,split2
+			--dpi-desync=fake,multidisorder
+			--dpi-desync-split-pos=1,midsld
 			--dpi-desync-repeats=11
 			--dpi-desync-fooling=md5sig
 			--dpi-desync-fake-tls=/opt/zapret/files/fake/tls_clienthello_www_google_com.bin
@@ -53,11 +54,11 @@ function set_cfg_default_values
 			--dpi-desync=fake
 			--dpi-desync-repeats=11
 			--new
-			--filter-tcp=80,443 <HOSTLIST>
-			--dpi-desync=fake,disorder2
+			--filter-tcp=443 <HOSTLIST>
+			--dpi-desync=fake,multidisorder
+			--dpi-desync-split-pos=midsld
 			--dpi-desync-repeats=6
-			--dpi-desync-autottl=2
-			--dpi-desync-fooling=md5sig
+			--dpi-desync-fooling=badseq,md5sig
 		"
 		# save changes
 		commit $cfgname
