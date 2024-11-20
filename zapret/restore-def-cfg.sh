51fc9739d811f94b01c7b9a1bb6b629129268f95
remittor <remittor@gmail.com>
2024-11-20 22:08:40 +0300

    config: Skip run_on_boot option on restore default uci-config

--- zapret/restore-def-cfg.sh
+++ zapret/restore-def-cfg.sh
@@ -3,7 +3,14 @@
 
 . /opt/zapret/comfunc.sh
 
+cfg_run_on_boot="$( uci -q get zapret.config.run_on_boot )"
+
 create_default_cfg
+
+if [ "$cfg_run_on_boot" = "1" ]; then
+	uci set zapret.config.run_on_boot=1
+	uci commit
+fi
 
 if [ "$1" = "sync" ]; then
 	# renew main config
